755575f212aa77c541ee6cdefdbf70f74fe6493a
Segundavid-Dev <segdavid03@gmail.com>
2025-09-11 23:15:59 +0100

    style: add fixed height

---
+++ entrypoints/popup/screens/onboard-screen.tsx
@@ -0,0 +1,10 @@
+import { ShieldCheck } from "lucide-react";
+
+export default function OnboardScreen() {
+  return (
+    <div>
+      <ShieldCheck size={64} />
+      <h1>Ad-Shield</h1>
+    </div>
+  );
+}
