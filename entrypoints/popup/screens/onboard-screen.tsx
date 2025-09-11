456edaef214385c13d3aaf0189c635baf5078607
Segundavid-Dev <segdavid03@gmail.com>
2025-09-12 00:14:18 +0100

    unistall tailwind

--- entrypoints/popup/screens/onboard-screen.tsx
+++ entrypoints/popup/screens/onboard-screen.tsx
@@ -2,7 +2,7 @@ import { ShieldCheck } from "lucide-react";
 
 export default function OnboardScreen() {
   return (
-    <div className="bg-red-500">
+    <div>
       <ShieldCheck size={64} />
       <h1>Ad-Shield</h1>
     </div>
