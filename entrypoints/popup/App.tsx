beb18060a899fa92f39bba7ec07e01635ae43a23
Segundavid-Dev <segdavid03@gmail.com>
2025-09-11 23:31:50 +0100

    added splash screen effect

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 import OnboardScreen from "./screens/onboard-screen";
 
 function App() {
+  const [showSplash, setShowSplash] = useState<boolean>(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    }, 2000);
+
+    // return cleanup function
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div>
-      <OnboardScreen />
-    </div>
+    <div>{showSplash ? <OnboardScreen /> : <div>Main App Content</div>}</div>
   );
 }
 
