19c01c29606fd67c33a39d330d4604f11a7139e8
Segundavid-Dev <segdavid03@gmail.com>
2025-09-12 06:50:02 +0100

    add tooltip component

--- entrypoints/popup/components/footer.tsx
+++ entrypoints/popup/components/footer.tsx
@@ -1,21 +1,36 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+  TooltipProvider,
+} from "@/components/ui/tooltip";
 import { Info } from "lucide-react";
 
 export default function Footer() {
   return (
-    <div style={{ backgroundColor: "#1a1a1a", padding: "10px" }}>
-      <p>
-        Turn on Ad Shield. It helps annoying Ads and unsafe element to provide
-        you with a clean and safe browsering environment.
-        <span
-          style={{
-            marginLeft: "8px",
-            verticalAlign: "middle",
-            cursor: "pointer",
-          }}
-        >
-          <Info size={16} />
-        </span>
-      </p>
-    </div>
+    <TooltipProvider>
+      <div style={{ backgroundColor: "#1a1a1a", padding: "10px" }}>
+        <p>
+          Turn on Ad Shield. It helps annoying Ads and unsafe element to provide
+          you with a clean and safe browsering environment.
+          <span
+            style={{
+              marginLeft: "8px",
+              verticalAlign: "middle",
+              cursor: "pointer",
+            }}
+          >
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Info size={16} />
+              </TooltipTrigger>
+              <TooltipContent side="bottom">
+                <p>Blocks ads • Protect Privacy</p>
+              </TooltipContent>
+            </Tooltip>
+          </span>
+        </p>
+      </div>
+    </TooltipProvider>
   );
 }
