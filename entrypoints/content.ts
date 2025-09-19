3299ffe055c84d22f64a8881d087363081df72af
Segundavid-Dev <segdavid03@gmail.com>
2025-09-19 17:28:12 +0100

    feat: removing popups and handling redirect

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,6 +1,54 @@
 export default defineContentScript({
-  matches: ['*://*.google.com/*'],
+  matches: ["<all_urls>"],
+  runAt: "document_idle",
   main() {
-    console.log('Hello content.');
+    console.log("Ad blocker content script started.");
+
+    let lastUserClick = 0;
+
+    document.addEventListener("click", () => {
+      lastUserClick = Date.now();
+    });
+
+    const userJustClicked = () => {
+      return Date.now() - lastUserClick < 2000;
+    };
+
+    // Intercept navigation attempts
+    window.addEventListener("beforeunload", (e) => {
+      if (!userJustClicked()) {
+        console.log("Blocked suspicious redirect.");
+        e.preventDefault();
+      }
+    });
+
+    // intercept JS-based redirects
+    const originAssign = window.location.assign;
+    window.location.assign = function (url: string) {
+      if (!userJustClicked()) {
+        console.log("Blocked JS redirect to:", url);
+      } else {
+        originAssign.call(window.location, url);
+      }
+    };
+
+    // remove popups banners
+    function removePopups() {
+      const popups = document.querySelectorAll("div, iframe");
+
+      popups.forEach((el) => {
+        const style = window.getComputedStyle(el);
+
+        if (
+          (style.position === "fixed" || style.position === "absolute") &&
+          parseInt(style.zIndex) > 1000 &&
+          el.clientHeight > window.innerHeight * 0.5
+        ) {
+          el.remove();
+        }
+      });
+    }
+
+    setInterval(removePopups, 1000);
   },
 });
--- wxt.config.ts
+++ wxt.config.ts
@@ -4,12 +4,6 @@ import { defineConfig } from "wxt";
 export default defineConfig({
   modules: ["@wxt-dev/module-react"],
   manifest: {
-    content_scripts: [
-      {
-        matches: ["<all_urls>"], // inject into every website
-        js: ["content.ts"],
-        run_at: "document_idle",
-      },
-    ],
+    // add permissions background + network later
   },
 });
